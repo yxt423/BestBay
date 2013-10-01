b8c325e4039c132f12740c5990bda9b8432ec8a5
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-09-30 19:00:31 -0700

    Adding in initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NaVi::Application.config.secret_token = '8740a09315b1251da29e94292e3e562dff28010c2ddee93f6aca11d4c9e2735d935d35a6ae4feefef211beda4523744ccc4cd7ad887bcfe1341e98ce81da70e6'
