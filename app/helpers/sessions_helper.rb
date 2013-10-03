6d65de9142d34aab4158a0afb77e5c37c846b5e8
yxt423 <yxt423@gmail.com>
2013-10-02 22:15:49 -0400

    change to ruby1.9.3 rails 3.2.14

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,7 +13,7 @@ module SessionsHelper
 
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
+    @current_user ||= User.find_by_remember_token(remember_token)
   end
 
   def current_user?(user)
