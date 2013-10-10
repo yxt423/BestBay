38a067a7a57bf40d7a4862e0119a8999b9fa5ff4
Rohan <rohan@localhost.localdomain>
2013-10-10 18:19:30 -0400

    Added list of items on user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,8 @@
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    # creating an items array to get users all items
+    @items = current_user.items
 
     respond_to do |format|
       format.html # show.html.erb
