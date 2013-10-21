fda8b6f1d020bcc2d41ac9e9fdae03c50c2ea87b
yxt423 <yxt423@gmail.com>
2013-10-21 12:20:41 -0400

    Xintong | correct signup_page test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,27 +2,42 @@ require 'spec_helper'
 
 describe 'User pages' do
 
-  #before do
-  #  @user = User.new(fname: 'Example',lname: "User", email: "user@example.com", password: "111111", password_confirmation:"111111",is_seller:true)
+  subject { page }
+
+  #describe "profile page" do
+  #  pending
+  #  let(:user) { FactoryGirl.create(:user) }
+  #  before { visit user_path(user) }
+  #
+  #  it { should have_content(user.name) }
+  #  it { should have_title(user.name) }
   #end
 
-  describe "signup page" do
+  describe "signup" do
     before { visit '/signup' }
-    subject { page }
+    let(:submit) { "Create my account" }
+
     it_behaves_like 'static page','/signup','Sign Up','Sign Up'
 
-    it 'fill in' do
-
-
-
-    fill_in "Confirm Password", with: "foobar"
-    fill_in "Email",            with: "user@example.com"
-    #fill_in "First Name",       with: "Example"
-    #fill_in "Last Name",        with: "User"
-    click_button "Create my account"
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
     end
-    # test seller option
-    #it { should have_content('sell') }
+
+    describe "with valid information" do
+      before do
+        fill_in "user_fname",                 with: "Example"
+        fill_in "user_lname",                 with: "User"
+        fill_in "user_email",                 with: "user@example.com"
+        fill_in "user_password",              with: "foobar"
+        fill_in "user_password_confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 
 
