b97dd457e994fdc7631fd91dc0b2d64f95bf4df4
Sohil Habib <vsohail09@gmail.com>
2013-10-21 19:49:13 -0400

    SY | Browse By Category Complete

--- spec/requests/item_pages_spec.rb
+++ spec/requests/item_pages_spec.rb
@@ -7,30 +7,35 @@ describe 'Test pages' do
   describe 'should show correct items by category' do
 
     subject { page }
-    before { visit '/home' , pending}
+    before { visit '/home'}
     it "should have the right title" do
 
-      expect(page).to should redirect_to items_path
+      page.should have_title("All Items")
     end
 
 
     it { should have_selector('h1',    text: 'All Items') }
 
 
-    it "should have some entries" do
-      expect { click_link "All" }.to should have_title("All Items")
+    it "should have All Items title" do
+      click_link "All"
+
+      page.should have_title("All Items")
+    end
+
+    it "should have book title" do
+      click_link "Books"
+      page.should have_title("Books")
+    end
+
+    it "should have Phone title" do
+      click_link "Phones"
+      page.should have_title("Phones")
     end
 
     it "should have some entries" do
-      expect { click_link "Books" }.to should have_title("Books")
-    end
-
-    it "should have some entries" do
-      expect { click_link "Phones" }.to should have_title("Phones")
-    end
-
-    it "should have some entries" do
-      expect { click_link "Other" }.to should have_title("Others")
+      click_link "Other"
+      page.should have_title("Other")
     end
 
   end
