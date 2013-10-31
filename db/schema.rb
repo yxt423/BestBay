5d06c556761da9c152a414deae99a08dead13686
yxt423 <yxt423@gmail.com>
2013-10-31 19:45:58 -0400

    delete seller check for sell button

--- db/schema.rb
+++ db/schema.rb
@@ -59,8 +59,8 @@ ActiveRecord::Schema.define(:version => 20131023221520) do
     t.string   "credit_card_no"
     t.boolean  "is_seller"
     t.string   "profile_pic"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "remember_token"
     t.integer  "expiry_month"
     t.integer  "expiry_year"
