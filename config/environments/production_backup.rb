90840d0824b8fe5671ee604a76858e44005af8a5
Rohan Honwade <rohan@mail.com>
2013-11-18 11:14:06 -0500

    Rohan | Mail settings for production server deployment

--- config/environments/production_backup.rb
+++ config/environments/production_backup.rb
@@ -66,14 +66,4 @@ NaVi::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.eager_load = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { 
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'gmail.com',
-      user_name:            'bullfrog.game',
-      password:             ENV['SMTP_PASSWORD'], 
-      authentication:       'plain',
-      enable_starttls_auto: true  }
-  config.action_mailer.perform_deliveries = true
 end
